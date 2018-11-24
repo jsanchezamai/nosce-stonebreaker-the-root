379f5b927509e1c213ff539152c96f73d863ce87
ox <ox@ismy.band>
2018-11-24 10:36:57 +0100

    Start to deploy

---
+++ pretty-printing-the-tutorial/README.md
@@ -0,0 +1,10 @@
+On this folder we are pretty printing tutorials chapters.
+
+See original raw sources:
+
+- [.md](../raw-nosce-tutorial.md)
+- [.html](../raw-nosce-tutorial.html)
+
+See advance:
+
+- [.md](./raw-nosce-tutorial-00.md)
---
+++ scripts/shell-commands.sh
@@ -0,0 +1,23 @@
+#
+# This script is part of:
+# https://martinsantome.github.io/nosce-stonebreaker-the-root/
+#
+# It is intended to be used by a bot:
+# https://denisbot.herokuapp.com/
+#
+# This file is a feed for the boot.
+#
+# The structure of this file is "ulterance/intent"
+#
+# echo are ulterances
+# commented line is the intent
+#
+# Please fork or better pull request.
+
+echo "Upgrade Linux reverse shell"
+# python -c "import pty; pty.spawn(‘/bin/bash’)"
+
+echo "How to background a process"
+# keyboard shortcut: Ctrl+Z
+
+
