06cc290485181fab8825f964a15a85c1df82ac19
ox <ox@ismy.band>
2018-11-25 00:19:40 +0100

    Create an interactive simulations corner

--- index.md
+++ index.md
@@ -1,2 +1,9 @@
 - [The Raw Thing](./raw-nosce-tutorial.html).
 - [Do-the-upgrade-your-Linux-reverse-shell thing](./scripts/the-root.md).
+
+Simulations
+-----------
+All the explanations on this project are supported by interactive
+2D simulation scena provided by [PhET](https://docs.google.com/document/d/1Ys1EiwnqQGYuzGOcQSr4uXDes35mF1v1XhMZIl10nk8/edit#bookmark=id.ktug2sc8jabe).
+
+See [simulations](./simulations) to get the point.
---
+++ install.sh
@@ -0,0 +1,23 @@
+echo "This document will follow: https://docs.google.com/document/d/1Ys1EiwnqQGYuzGOcQSr4uXDes35mF1v1XhMZIl10nk8/edit#bookmark=id.ktug2sc8jabe"
+# to install an interactive simulator editor in your environment.
+
+git clone https://github.com/phetsims/example-sim.git
+git clone https://github.com/phetsims/assert.git
+git clone https://github.com/phetsims/axon.git
+git clone https://github.com/phetsims/brand.git
+git clone https://github.com/phetsims/chipper.git
+git clone https://github.com/phetsims/dot.git
+git clone https://github.com/phetsims/joist.git
+git clone https://github.com/phetsims/kite.git
+git clone https://github.com/phetsims/perennial.git
+git clone https://github.com/phetsims/phet-core.git
+git clone https://github.com/phetsims/phetcommon.git
+git clone https://github.com/phetsims/query-string-machine.git
+git clone https://github.com/phetsims/scenery.git
+git clone https://github.com/phetsims/scenery-phet.git
+git clone https://github.com/phetsims/sherpa.git
+git clone https://github.com/phetsims/sun.git
+git clone https://github.com/phetsims/tandem.git
+
+
+
